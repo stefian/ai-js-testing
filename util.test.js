c12779ce0a9bb3ae5f4e172091c2e2adf7128280
stefian <v_ianta@yahoo.com>
2019-06-15 10:51:46 -0400

    added 2nd set of vals for first test to avoid false positives

--- util.test.js
+++ util.test.js
@@ -3,6 +3,8 @@ const { generateText } = require('./util');
 test('should output name and age', () => {
     const text = generateText('Max', 29);
     expect(text).toBe('Max (29 years old)');
+    const text2 = generateText('Ada', 28);
+    expect(text2).toBe('Ada (28 years old)');
 });
 
 test('should output data-less text', () => {
